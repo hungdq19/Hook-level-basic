687bbcbb5d3a572923bee3a8fd22c3c65827c3ea
Hung Do <doquochung09091993@gmail.com>
2021-03-16 00:19:13 +0700

    Setting API and TEST Call API

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
+import { useEffect } from 'react';
 import { Route, Switch } from 'react-router';
 import { NavLink } from 'react-router-dom';
+import userApi from './Api/userApi';
 import './App.css';
 import HomePage from './component/HomePAge';
 import AlbumFeature from './features/Album';
 import TodoFeatue from './features/Todo';
 
 function App() {
+  useEffect(() =>{
+    const callAPI = async () =>{
+      const userList = await userApi.get()
+      console.log(userList)
+    }
+    callAPI();
+  },[])
   // style cho thẻ link để xuống dòng
   const link = {
     display:"block",
